83b4902bd69f7a46de9acdf6cccd96c9c67e3c67
venu <yedukondalunaik12345@gmail.com>
2023-06-16 20:46:15 +0530

    just opened the tic_tac_toe folder

--- tic_tac_toe/src/App.jsx
+++ tic_tac_toe/src/App.jsx
@@ -1,10 +1,18 @@
-
+import { useState } from 'react'
 import '../style.scss'
-import Board from './components/Board'
+// import Board from './components/Board'
 function App() {
+  const [counter,setCount]=useState(1); //the use states returns an array of values
+  
+  const setCounter=()=>{
+    setCount((counter)=>{return counter+1});
+  }
   return (
     <div className='app'>
-   <Board/>
+   <button type="button" onClick={setCounter}>
+    click me
+   </button>
+   <h1>{counter}</h1>
     </div>
   )
 }
--- tic_tac_toe/src/components/Square.jsx
+++ tic_tac_toe/src/components/Square.jsx
@@ -1,6 +1,8 @@
 const Square=({value})=>{
  return(
-        <><button type="button" className="square">{value}</button></>
+        <><button type="button" onClick={(event)=>{
+                console.log(event);
+        }}className="square">{value}</button></>
         )
 }
 export default Square;
